eb6bf72e723cc9f9ddd3516bb8dba65b4b6a40a3
byronskout <vyskoutaris@hotmail.com>
2019-11-19 09:52:04 +0000

    User model

--- app/User.php
+++ app/User.php
@@ -2,19 +2,12 @@
 
 namespace App;
 
-// Namespace Imports...
-
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
     use Authenticatable, Authorizable, CanResetPassword;
 
-    // Other Eloquent Properties...
-
-    /**
-     * Get all of the tasks for the user.
-     */
     public function tasks()
     {
         return $this->hasMany(Task::class);
